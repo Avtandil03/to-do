b413bb96cfe4ab5d50e0e0a6bc7aad69bf649195
Avtandil <avtandilqazxswedc@gmail.com>
2023-02-19 15:31:42 +0300

    i don't how it comment

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,21 @@ import RightSideBar from './pages/RightSideBar'
 import { RouterProvider, } from "react-router-dom";
 import { router } from './router/router';
 import MySwitch from './UI/MySwitch';
+import Settings from './pages/Settings';
+import Login from './pages/Login';
 
 function App() {
-  const [theme, setTheme]= useState('ligth')
+  const [isAuth, setIsAuth] = useState(false)
+  const [showSett, setShowSett] = useState(true)
 
-  useEffect(() => {
-    if(theme === 'dark' ){
-      document.documentElement.classList.add('dark')
-    }else{
-      document.documentElement.classList.remove('dark')
-    }
-  }, [theme])
+
 
   return (
-    <div className='flex justify-between h-screen w-screen'>
-      <LeftSideBar />
-    <MySwitch/>
-      
+    <div className='flex justify-between  w-screen '>
+      {/* <LeftSideBar />      
       <RouterProvider router={router} />
-      <RightSideBar />
+      <RightSideBar /> */}
+      {isAuth ? showSett && <Settings /> : <Login/>}
     </div>
   )
 }
