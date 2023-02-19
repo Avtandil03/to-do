b12920e56d193ce69d9920fff0b31f2caca96fff
Avtandil <avtandilqazxswedc@gmail.com>
2023-02-19 15:28:29 +0300

    improve mySwitch

--- src/UI/MySwitch.jsx
+++ src/UI/MySwitch.jsx
@@ -4,8 +4,8 @@ export default function MySwitch({onText = 'On', offText = 'Off', changed}) {
   const [enabled, setEnabled] = useState(false);
 
   return (
-      <div className="flex">
-        <label className="inline-flex relative items-center mr-5 cursor-pointer">
+      <div className="flex w-[72px]">
+        <label className="inline-flex relative items-center mr-5 cursor-pointer justify-between">
           <input
             type="checkbox"
             className="sr-only peer"
@@ -22,13 +22,13 @@ export default function MySwitch({onText = 'On', offText = 'Off', changed}) {
               bg-transparent peer-checked:bg-sky-400 after:transition-all
               border border-gray-300 peer-checked:border-transparent
               after:content-[''] after:absolute after:left-[1px] peer-checked:after:translate-x-5
-              after:w-3.5 hover:after:w-4 after:h-3.5 hover:after:h-4 
+              after:w-3 hover:after:w-3.5 after:h-3 hover:after:h-3.5 
               after:bg-gray-300 peer-checked:after:bg-black after:rounded-full
               "
           ></div>
           <span className="ml-2 text-sm font-medium text-gray-900">
               { enabled ? onText : offText}
-            </span>
+          </span>
         </label>
       </div>
   );
