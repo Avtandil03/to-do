b190e59aab83fe2a43f4c92ccf37d06953d4adfe
Avtandil <avtandilqazxswedc@gmail.com>
2023-02-19 15:29:45 +0300

    fix bug & custom scrollbar

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,16 +9,27 @@ module.exports = {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  darkmode: "class",
+  darkMode: "class",
   theme: {
     extend: {},
+    colors: {
+      transparent: 'transparent',
+      'white': '#fff',
+      'black': '#000',
+      'main-bg': '#f3f3f3',
+      'btn-blue': '#436af2',
+      'txt-gray': '#656565'
+    },
+    fontSize: {
+      sm: '0.8rem',
+      base: '1rem',
+      lg: ['18px', '20px'],
+      xl: '1.25rem',
+      '2xl': '1.563rem',
+      '3xl': '1.953rem',
+      '4xl': '2.441rem',
+      '5xl': '3.052rem',
+    }
   },
   plugins: [],
 }
-
-
-// module.exports = {
-//   theme: {
-    
-//   },
-// }
